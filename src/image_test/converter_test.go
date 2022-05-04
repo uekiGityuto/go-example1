e50182390087ca14729398882dd7074da9f4649f
ueki-yuto <ueki-yuto@dmm.com>
2022-05-05 05:05:49 +0900

    単体テスト修正

---
+++ src/image_test/converter_test.go
@@ -0,0 +1,20 @@
+package image_test_test
+
+import (
+	"github.com/uekiGityuto/go-example/src/image"
+	"testing"
+)
+
+func TestJPEGToPNG_正常系(t *testing.T) {
+	converter := image.Converter{Directory: "testdata"}
+	if err := converter.JPEGToPNG(); err != nil {
+		t.Errorf("画像変換失敗。%s", err)
+	}
+}
+
+func TestJPEGToPNG_異常系_存在しないディレクトリ(t *testing.T) {
+	converter := image.Converter{Directory: "illegal_directory"}
+	if err := converter.JPEGToPNG(); err == nil {
+		t.Errorf("存在しないディレクトリを指定してもエラーにならない。")
+	}
+}
